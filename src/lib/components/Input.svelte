89d355f64903dc7e6d3261a281ffe2185171ca6d
statiolake <statiolake@gmail.com>
2024-09-25 02:11:49 +0900

    feat: improve focus animation

--- src/lib/components/Input.svelte
+++ src/lib/components/Input.svelte
@@ -13,6 +13,7 @@
 	}>();
 
 	let inputElement: HTMLInputElement;
+	let isFocused = false;
 
 	export function focus() {
 		inputElement.focus();
@@ -26,16 +27,46 @@
 	function onKeyPress(event: KeyboardEvent) {
 		dispatch('keypress', event);
 	}
+
+	function onFocus() {
+		isFocused = true;
+	}
+
+	function onBlur() {
+		isFocused = false;
+	}
 </script>
 
-<input
-	bind:this={inputElement}
-	{type}
-	{placeholder}
-	{disabled}
-	{value}
-	on:input={onInput}
-	on:keypress={onKeyPress}
-	class={`pl-10 pr-12 py-6 w-full bg-white bg-opacity-20 border-2 border-gray-600 rounded-lg text-gray-800 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:border-transparent text-xl ${className}`}
-	{...$$restProps}
-/>
+<div class="relative">
+	<input
+		bind:this={inputElement}
+		{type}
+		{placeholder}
+		{disabled}
+		{value}
+		on:input={onInput}
+		on:keypress={onKeyPress}
+		on:focus={onFocus}
+		on:blur={onBlur}
+		class={`
+			pl-10 pr-12 py-6 w-full
+			bg-white bg-opacity-20
+			border-2 border-gray-600
+			rounded-lg text-gray-800
+			placeholder-gray-500
+			focus:outline-none
+			transition-all duration-200 ease-in-out
+			${className}
+		`}
+		{...$$restProps}
+	/>
+	<div
+		class="
+			absolute inset-0
+			rounded-lg
+			pointer-events-none
+			transition-all duration-200 ease-in-out
+			{isFocused ? 'ring-2 ring-gray-600' : ''}
+		"
+	></div>
+</div>
